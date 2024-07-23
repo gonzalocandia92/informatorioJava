06bb5d1b8f59c23a0c84b6bb3baf4f4e64b6f7ef
gonzalocandia92 <gonzaloismael.cg@gmail.com>
2024-07-22 22:24:45 -0300

    Clase 3

---
+++ TP_java/estructurado/EstructuraIf.java
@@ -0,0 +1,13 @@
+package estructurado;
+
+public class EstructuraIf {
+    public static void main(String[] args) {
+
+        int topPuntaje = 90;
+
+        if (topPuntaje< 100) {
+            System.out.println("Top puntaje es menor que 100.");
+        }
+
+    }
+}
